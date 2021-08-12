827aa55bb381af226150315b1508cec0b22811aa
nguyenlocdanh-clis <nguyen.loc.danh@clis.co.jp>
2021-08-12 13:13:22 +0900

    Update SampleAction.java

--- src/main/java/jp/gihyo/jenkinsbook/action/SampleAction.java
+++ src/main/java/jp/gihyo/jenkinsbook/action/SampleAction.java
@@ -72,10 +72,9 @@ public class SampleAction {
         //Select the greeting message according to the time
         Calendar calendar = Calendar.getInstance();
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
-        if (hour < PM_OF_HOUR){
+        if(hour < PM_OF_HOUR){
             dto.setMessage("Good morning");
-        }
-        else {
+        }else {
             dto.setMessage("Good afternoon");
         }
 
